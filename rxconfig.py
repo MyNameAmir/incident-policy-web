db4b5bec06850c9d7641bc4973169a3036cc52be
Amir <60043971+MyNameAmir@users.noreply.github.com>
2026-02-19 21:13:40 -0500

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,9 +2,13 @@ import reflex as rx
 
 config = rx.Config(
     app_name="incident_policy_web",
-    allowed_hosts=["incident-policy-web.onrender.com"],
+    allowed_hosts=[
+        "incident-policy-web.onrender.com",
+        "localhost",
+        "127.0.0.1",
+    ],
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
-    ]
+    ],
 )
